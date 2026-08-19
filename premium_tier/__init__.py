36d2391568270b26354321b87d4996537148d9f5
sudarsh-daidipya <sudarsh06@gmail.com>
2026-08-19 19:07:48 +0530

    Initialize premium tier module with imports

---
+++ premium_tier/__init__.py
@@ -0,0 +1,9 @@
+from premium_tier.matchmaker import initialize_similarity_engine
+from premium_tier.premium_ui import render_premium_dashboard
+from premium_tier import ai_integration
+
+__all__ = [
+    "initialize_similarity_engine",
+    "render_premium_dashboard",
+    "ai_integration"
+]
