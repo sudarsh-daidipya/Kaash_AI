f9a2ef2f71e3d515032febce43cea139fb54b616
sudarsh-daidipya <sudarsh06@gmail.com>
2026-08-19 19:17:59 +0530

    Update __init__.py

--- premium_tier/__init__.py
+++ premium_tier/__init__.py
@@ -1,10 +1,9 @@
-from premium_tier.matchmaker import initialize_similarity_engine, render_matchmaker_ui
+from premium_tier.matchmaker import initialize_similarity_engine
 from premium_tier.premium_ui import render_premium_dashboard
-from premium_tier.ai_integration import render_ai_analysis
+from premium_tier import ai_integration
 
 __all__ = [
     "initialize_similarity_engine",
-    "render_matchmaker_ui",
     "render_premium_dashboard",
-    "render_ai_analysis"
+    "ai_integration"
 ]
